eb2247b2dc68c35378de4914e39da186b695753e
kkobala <kobal@DESKTOP-IBUNFC5>
2023-03-06 11:08:35 +0400

    modified

--- BankSystem/Models/Operator.cs
+++ BankSystem/Models/Operator.cs
@@ -1,14 +1,8 @@
 ﻿namespace BankSystem.Models
 {
-<<<<<<< HEAD
     public class Operator
     {
         public int Id { get; set; }
         public string Name { get; set; }
     }
-=======
-	public class Operator
-	{
-	}
->>>>>>> b0aff5f66fde4e23de354c9b9706504269ea254d
 }

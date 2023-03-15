fcd9710360367fbb205ed61419bc661c08bf7bf8
Bazu98 <v.bazuashvili@gmail.com>
2023-03-15 06:19:39 -0700

    final version of atm

--- BankSystem/Models/User.cs
+++ BankSystem/Models/User.cs
@@ -4,10 +4,10 @@ namespace BankSystem.Models
 {
 	public class User
 	{
-		public int Id { get; set; }
+		
 		public string? Name { get; set; }
 		public string? LastName { get; set; }
-		public DateTime RegisteredAt { get; set; }
+	
 		public string PersonalNumber { get; set; }
 		public string? Email { get; set; }
 		public string BirthDate { get; set; }
